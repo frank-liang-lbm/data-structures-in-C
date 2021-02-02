5db2130869e9d93c7473490ab6c1e1811c4c5d5a
Frank Liang <76883265+frank-liang-lbm@users.noreply.github.com>
2021-02-02 10:12:47 +0800

    Add files via upload

---
+++ maxsubsequence.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int Maxsubsequence(const A[],int N);
+int main(void)
+{
+	int K,i,Maxsum;
+	int A[K];
+	scanf("%d",&K);
+	for(i=0;i<K;i++)
+	{
+		scanf("%d",&A[i]);
+	}
+	Maxsum=Maxsubsequence(A,K);
+	printf("%d\n",Maxsum);
+	return 0;
+}
+
+int Maxsubsequence(const A[],int N)
+{
+	int ThisSum,MaxSum,i,j,k;
+	MaxSum=0;
+	for(i=0;i<N;i++)
+	{
+		for(j=i;j<N;j++)
+		{
+			ThisSum=0;
+			for(k=i;k<=j;k++)
+				ThisSum+=A[k];
+			if(ThisSum>MaxSum)
+				MaxSum=ThisSum; 
+		}
+	
+	}
+	return MaxSum;
+	
+}
